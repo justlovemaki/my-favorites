0c2a07666d67a53a6a9dbc701b817a8b45fef3d4
justlovemaki <274166795@qq.com>
2025-05-19 18:20:35 +0800

    feat(web): 新增独立前端版本并优化GitHub Token管理
    添加独立前端HTML/CSS/JS文件，支持通过nginx部署
    实现GitHub Token管理功能，包括设置、清除和缓存处理
    优化错误处理和缓存机制，增加重新加载按钮
    修改README说明独立前端和Token功能
    移除不再需要的后端代理代码，直接使用fetch API

--- readme.md
+++ readme.md
@@ -3,23 +3,26 @@
 基于github的网络收藏夹
 
 ## 简介
-* 基于SpringBoot2+Vue的服务端
+* 基于SpringBoot2后端+Vue前端
 * 通过Github Api获取书签数据
 * js解析root目录下md文件，生成书签目录
 * 使用ConverBookMarksKit，可转换chrome,360导出的书签为md
 * 支持多级目录嵌套，默认5级
 * sessionStorage作为本地缓存
 * 支持隐藏显示书签目录
+* 支持私有github仓库
+* 配套可增删改的chrome插件 src/chrome-ext
+* 独立前端代码，配合nginx重写路径 src/html
 
 ## 使用
 * fork项目到你的github仓库
 * 修改root目录下的md文件
 * 修改"* ",星号越多层级越多
 * hide.md结尾的文件，默认隐藏
-* 搜索框输入89757显示隐藏
-* 访问路径 http://web.myfav.online/{你的仓库根路径}
-* url最后加上#tk={token}可通过token访问私有仓库内容
-* [试用一下吧]( http://web.myfav.online)
+* 搜索框输入89757显示隐藏,可修改showkey文件来改变显示的key
+* Demo访问路径 https://book.justlikemaki.vip/favorites/{你的仓库根路径}
+* 可通过设置token访问私有仓库内容
+* [试用一下吧](https://book.justlikemaki.vip/favorites/)
 
 ## Issue
 * 欢迎您的更多反馈
--- src/main/java/com/fav/util/ConverBookMarksKit.java
+++ src/main/java/com/fav/util/ConverBookMarksKit.java
@@ -12,8 +12,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.IoUtil;
@@ -21,75 +19,9 @@ import cn.hutool.core.io.IoUtil;
 public class ConverBookMarksKit {
 	private static final String HOME = "/home/maki/桌面/marks/";
 
-	public static void main(String[] args) throws Exception {
-//		toMarkDown(getBookMarks(HOME + "test-mark-g.html"));
-//		toMarkDownAtavi(getBookMarks(HOME + "test-mark.html"));
-//		toBookMarksXml(HOME + "xxx.md");
-	}
-
-	private static String readTmpl(String source) throws Exception {
-		Resource resource = new ClassPathResource(source);
-		File sourceFile = resource.getFile();
-		return FileUtil.readString(sourceFile, "UTF-8");
-	}
-
-	public static void toBookMarksXml(String path) throws Exception {
-		int flagLevel = 0;
-		StringBuilder sb = new StringBuilder();
-		sb.append(readTmpl("bookmarks/head.html")).append("\r\n");
-		List<String> list = FileUtil.readLines(new File(path), "UTF-8");
-		for (int i = 0; i < list.size(); i++) {
-			String str = list.get(i);
-			int flag = getLevel(str);
-			if (flag < flagLevel) {
-				for (int j = 0; j < flagLevel - flag; j++) {
-					sb.append(readTmpl("bookmarks/end.html")).append("\r\n");
-				}
-			}
-			flagLevel = flag;
-			getTmplChild(str, sb);
-		}
-		sb.append(readTmpl("bookmarks/end.html")).append("\r\n");
-		sb.append(readTmpl("bookmarks/end.html")).append("\r\n");
-//		 System.out.println(sb.toString());
-		FileUtil.writeString(sb.toString(), new File(HOME + "dr.html"), "UTF-8");
-	}
-
-	private static void getTmplChild(String str, StringBuilder sb) throws Exception {
-		String text = null;
-		String herf = null;
-		int bgText = str.indexOf("* [");
-		int edText = str.indexOf("](");
-		long time = System.currentTimeMillis() / 1000;
-		if (str.endsWith("(dir)")) {
-//			 System.out.println(str.substring(bgText+3,edText));
-			String tmpl = readTmpl("bookmarks/dir.html");
-			sb.append(tmpl.replace("{{time}}", time + "").replace("{{text}}", str.substring(bgText + 3, edText)))
-					.append("\r\n");
-			sb.append(readTmpl("bookmarks/bg.html")).append("\r\n");
-		} else {
-//			 System.out.println(str.substring(bgText+3,edText));
-//			 System.out.println(str.substring(edText+2,str.length()-1));
-			String tmpl = readTmpl("bookmarks/item.html");
-			sb.append(
-					tmpl.replace("{{time}}", time + "").replace("{{href}}", str.substring(edText + 2, str.length() - 1))
-							.replace("{{text}}", str.substring(bgText + 3, edText)))
-					.append("\r\n");
-		}
-	}
-
-	private static int getLevel(String str) {
-		StringBuilder xin = new StringBuilder();
-		for (int i = 0; i < 8; i++) {
-			xin.append("* ");
-		}
-		for (int i = xin.length(); i >= 0; i = i - 2) {
-			if (str.indexOf(xin.toString()) == 0) {
-				return xin.length() / 2;
-			}
-			xin.delete(0, 2);
-		}
-		return 1;
+	public static void main(String[] args) {
+		// toMarkDown(getBookMarks(HOME + "test-mark.html"));
+		toMarkDownAtavi(getBookMarks(HOME + "test-mark.html"));
 	}
 
 	private static InputStream getBookMarks(String path) {
@@ -123,6 +55,7 @@ public class ConverBookMarksKit {
 			Elements list = bgEle.children();
 			List<Element> nonameList = new ArrayList<Element>();
 			for (int i = 0; i < list.size(); i++) {
+				// for (int i = 2; i < 3; i++) {
 				Element element = list.get(i);
 				if (StringUtils.isNotBlank(element.html())) {
 					if (element.childNodeSize() == 2) {
@@ -162,7 +95,7 @@ public class ConverBookMarksKit {
 		StringBuilder level = new StringBuilder();
 		level.append("* ");
 		foreachIterator(it, sb, level);
-//		 System.out.println(sb.toString());
+		// System.out.println(sb.toString());
 		FileUtil.writeString(sb.toString(), new File(HOME + mdFileName + ".md"), "utf-8");
 	}
 
